106163c1e0457d97d9a6ef209be0cb1b7971c73d
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2022-10-11 11:58:53 +0200

    feat: another example of run order

---
+++ src/runorder.ts
@@ -0,0 +1,19 @@
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function run(value: number) {
+  console.log("run(" + value + ") - This is synchronous");
+  await delay(100);
+  console.log("run(" + value + ") - This is asynchronous, because it's after `await`");
+}
+
+async function test(){
+  console.log("Print 1");
+  run(1);       // <== WITHOUT await
+  console.log("Print 2");
+  await run(2); // <== With await
+  console.log("Print 3");
+}
+
+test();
